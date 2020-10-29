d1a2b93265e07bdf2fbc7ce74d669126147a3d75
DAngelo Thomas <dangelo1thomas@gmail.com>
2020-10-29 16:29:39 -0400

    commit tree 28d8116f0e3078b5631a4133a35f706ece728ad7

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num) 
+    if num <= 1
+        return false 
+else 
+    (2..(num - 1)).each do |n|
+        return false if num % n == 0
+    end
+end
+    true
+
+end
